8ce69425065f2bb61a0b45a7afb22d7e85819456
mayagwright20 <mayagwright20@hotmail.com>
2024-07-20 14:35:16 +0100

    Calendar added

--- screens/CalendarScreen.tsx
+++ screens/CalendarScreen.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
+import { Calendar, LocaleConfig } from "react-native-calendars";
 import LinearBackground from "../components/layout/LinearBackground";
 import Header from "../components/layout/Header";
 import { ScrollView, StyleSheet } from "react-native";
 import { COLORS } from "../constants/Colors";
 
 export default function CalendarScreen() {
+  const [selected, setSelected] = useState("");
+
   return (
     <LinearBackground>
       <Header title="Calendar" />
       <ScrollView style={styles.scrollview}>
-        
+        <Calendar
+          onDayPress={(day) => {
+            setSelected(day.dateString);
+          }}
+          markedDates={{
+            [selected]: {
+              selected: true,
+              disableTouchEvent: true,
+              selectedTextColor: COLORS.MINT_GREEN,
+            },
+          }}
+        />
       </ScrollView>
     </LinearBackground>
   );
@@ -17,8 +31,11 @@ export default function CalendarScreen() {
 
 const styles = StyleSheet.create({
   scrollview: {
-    backgroundColor: "white",
+    backgroundColor: COLORS.WHITE,
     borderBottomColor: COLORS.LIGHT_GREY,
     borderBottomWidth: 1,
+    borderTopRightRadius: 30,
+    borderTopLeftRadius: 30,
+    paddingTop: 20,
   },
 });
